c44188b0c4b0230992c963b977d6b445306bbc4f
Dmitry Grigoriev <dmitry@dxfoto.ru>
2013-11-05 22:08:12 +0400

    Initial setup, gems, basic layout, landing page, user model draft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitPub::Application.config.secret_key_base = '1ef9c499467e8d6f1c189bd088729225c260883d19457ca3dfe97cd807b26d84b6e2f6ce4cd15cf4fcdd134b0d44eb378c91dfb681cac1af97ed485a5d821a66'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitPub::Application.config.session_store :cookie_store, key: '_GitPub_session'
