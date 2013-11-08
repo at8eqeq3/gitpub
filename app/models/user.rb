de5bb1df3254c4b2443870b9baebfc29a5091bf3
Dmitry Grigoriev <dmitry@dxfoto.ru>
2013-11-08 22:32:11 +0400

    fixed error when user logs in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,12 @@ class User < ActiveRecord::Base
       user.name       = auth['info']['name']
     end
   end
-  def self.update_with_omniauth(auth)
-    update! do |user|
-      user.gh_uid     = auth['uid']
-      user.gh_name    = auth['info']['name']
-      user.gh_login   = auth['info']['nickname']
-      user.gh_email   = auth['info']['email']
-      user.gh_profile = auth['info']['urls']['GitHub']
-      user.gh_website = auth['info']['urls']['Blog']
-    end
+  def update_with_omniauth(auth)
+    self.gh_uid     = auth['uid']
+    self.gh_name    = auth['info']['name']
+    self.gh_login   = auth['info']['nickname']
+    self.gh_email   = auth['info']['email']
+    self.gh_profile = auth['info']['urls']['GitHub']
+    self.gh_website = auth['info']['urls']['Blog']
   end
 end
